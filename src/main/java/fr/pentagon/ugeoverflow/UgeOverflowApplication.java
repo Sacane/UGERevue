38e2dba189708f3bec06ee44b514ac7817e5b093
Sacane <johan.ramaroson@gmail.com>
2023-12-22 17:00:58 +0100

    Init project back + front

---
+++ src/main/java/fr/pentagon/ugeoverflow/UgeOverflowApplication.java
@@ -0,0 +1,13 @@
+package fr.pentagon.ugeoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgeOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UgeOverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pentagon/ugeoverflow/UgeOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pentagon.ugeoverflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UgeOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
