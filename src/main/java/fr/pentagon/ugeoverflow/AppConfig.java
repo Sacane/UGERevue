fa0ee2535c596cae0c641a1f316f998ef778e555
Sacane <johan.ramaroson@gmail.com>
2024-01-12 17:18:11 +0100

    fixed to make project serve the front

---
+++ src/main/java/fr/pentagon/ugeoverflow/AppConfig.java
@@ -0,0 +1,16 @@
+package fr.pentagon.ugeoverflow;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/").addResourceLocations("classpath:/static/");
+    }
+}
