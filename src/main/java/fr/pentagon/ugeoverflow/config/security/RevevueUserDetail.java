9ee7a6312865bf56b83c422d338d4cd1c926af58
Sacane <johan.ramaroson@gmail.com>
2024-02-05 22:47:46 +0100

    Fixed lock account

--- src/main/java/fr/pentagon/ugeoverflow/config/security/RevevueUserDetail.java
+++ src/main/java/fr/pentagon/ugeoverflow/config/security/RevevueUserDetail.java
@@ -44,17 +44,17 @@ public class RevevueUserDetail implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
