110f75bbf67d9d67dc385de86e747c934fc14f82
Sacane <johan.ramaroson@gmail.com>
2024-02-10 14:35:14 +0100

    fixed merge

--- src/main/java/fr/pentagon/ugeoverflow/controllers/dtos/responses/QuestionDetailDTO.java
+++ src/main/java/fr/pentagon/ugeoverflow/controllers/dtos/responses/QuestionDetailDTO.java
@@ -3,8 +3,8 @@ package fr.pentagon.ugeoverflow.controllers.dtos.responses;
 import java.util.Date;
 import java.util.Objects;
 
-public record ReviewDTO(long id, String title, String javaFile, String testFile, long authorID, Date creationDate) {
-    public ReviewDTO {
+public record QuestionDetailDTO(long id, String title, String javaFile, String testFile, long authorID, Date creationDate) {
+    public QuestionDetailDTO {
         Objects.requireNonNull(title);
         Objects.requireNonNull(javaFile);
         Objects.requireNonNull(testFile);
