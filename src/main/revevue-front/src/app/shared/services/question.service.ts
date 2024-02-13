ef0a986179b6eeb6ad698b12636bc2760883f199
qtdrake <quentintellier01@yahoo.com>
2024-02-13 21:43:47 +0100

    Create question service for front

---
+++ src/main/revevue-front/src/app/shared/services/question.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, delay, of } from "rxjs";
+import { environment } from "../../environment";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class QuestionService {
+    private url: string = environment.apiUrl + '/questions';
+
+    constructor(private httpclient: HttpClient) { }
+
+    public deleteQuestion(questionId: string): Observable<any> {
+        const headers = new HttpHeaders().set('Content-Type', 'application/json');
+        console.log('SERVICE DELETE');
+
+        return of({ delete: true });
+        // return this.httpclient.delete<any>(this.url + '/questionId', { headers });
+    }
+}
