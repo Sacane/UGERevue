0c67910cf2db4a795d01ffcecfc4eae979995528
Sacane <johan.ramaroson@gmail.com>
2024-02-19 18:33:29 +0100

    Add provider for tests

--- src/test/java/fr/pentagon/ugeoverflow/controller/QuestionControllerTest.java
+++ src/test/java/fr/pentagon/ugeoverflow/controller/QuestionControllerTest.java
@@ -1,23 +1,61 @@
 package fr.pentagon.ugeoverflow.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.pentagon.ugeoverflow.DatasourceTestConfig;
+import fr.pentagon.ugeoverflow.controllers.LoginController;
+import fr.pentagon.ugeoverflow.controllers.QuestionController;
+import fr.pentagon.ugeoverflow.controllers.dtos.responses.QuestionDetailDTO;
+import fr.pentagon.ugeoverflow.exception.HttpExceptionHandler;
+import fr.pentagon.ugeoverflow.service.QuestionService;
+import fr.pentagon.ugeoverflow.service.UserService;
+import fr.pentagon.ugeoverflow.utils.Routes;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Import(DatasourceTestConfig.class)
 class QuestionControllerTest {
 
-//    private static final String DEFAULT_ROUTE = "/api/questions/";
-//
-//    private final ObjectMapper objectMapper = new ObjectMapper();
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    private final MockMvc questionMVC = MockMvcBuilders.standaloneSetup(new QuestionController(new FakeQuestionService()))
-//            .setControllerAdvice(new HttpExceptionHandler())
-//            .build();
+    private static final String DEFAULT_ROUTE = "/api/questions/";
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private QuestionService questionService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private MockMvc questionMVC;
+    private MockMvc loginControllerMock;
+
+    @BeforeEach
+    public void setup(){
+        var httpExceptionHandler = new HttpExceptionHandler();
+        loginControllerMock = MockMvcBuilders.standaloneSetup(new LoginController(authenticationManager))
+                .setControllerAdvice(httpExceptionHandler)
+                .build();
+        this.questionMVC = MockMvcBuilders.standaloneSetup(new QuestionController(questionService))
+                .setControllerAdvice(httpExceptionHandler)
+                .build();
+    }
 //
 //    @Autowired
 //    private LoginController loginController;
@@ -31,56 +69,56 @@ class QuestionControllerTest {
 //                .build();
 //    }
 //
-//    @Nested
-//    @Tag("GET")
-//    @DisplayName("Get all questions")
-//    final class GetAllQuestions {
-//
-//        private final List<QuestionDetailDTO> results;
-//
-//        private final ResultActions request;
-//
-//        GetAllQuestions() throws Exception {
-//            request = questionMVC.perform(MockMvcRequestBuilders.get(Routes.Question.ROOT).contentType(MediaType.APPLICATION_JSON));
-//            var responseBody = request.andReturn().getResponse().getContentAsString();
-//            var typeReference = new TypeReference<List<QuestionDetailDTO>>() {
-//            };
-//            this.results = objectMapper.readValue(responseBody, typeReference);
-//        }
-//
-//        @Test
-//        @DisplayName("Basic tests")
-//        void allOpenReviews() {
-//            assertAll(
-//                    () -> request.andExpect(MockMvcResultMatchers.status().isOk()),
-//                    () -> request.andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3)),
-//                    () -> assertEquals(3, results.size()),
-//                    () -> assertEquals(3, results.stream().distinct().count())
-//            );
-//        }
-//
-//        @ParameterizedTest
-//        @ValueSource(ints = {0, 1, 2})
-//        @DisplayName("IDs tests")
-//        void allOpenReviewsIDs(int index) {
-//            assertEquals(7L * index, results.get(index).id());
-//        }
-//
-//        @Test
-//        @DisplayName("Complex test")
-//        void allValuesAllOpenReviews() {
-//            assertAll(
-//                    () -> assertEquals("How to concat string in for statement", results.get(0).title()),
-//                    () -> assertEquals("StringBuilder", results.get(0).javaFile()),
-//                    () -> assertTrue(results.get(2).testFile().isEmpty()),
-//                    () -> assertEquals("@Test", results.get(1).testFile()),
-//                    () -> assertEquals(118218L, results.get(0).authorID()),
-//                    () -> assertEquals(3630L, results.get(1).authorID()),
-//                    () -> assertEquals(17L, results.get(2).authorID())
-//            );
-//        }
-//
-//    }
+    @Nested
+    @Tag("GET")
+    @DisplayName("Get all questions")
+    final class GetAllQuestions {
+
+        private final List<QuestionDetailDTO> results;
+        private final ResultActions request;
+
+
+        GetAllQuestions() throws Exception {
+            request = questionMVC.perform(MockMvcRequestBuilders.get(Routes.Question.ROOT).contentType(MediaType.APPLICATION_JSON));
+            var responseBody = request.andReturn().getResponse().getContentAsString();
+            var typeReference = new TypeReference<List<QuestionDetailDTO>>() {
+            };
+            this.results = objectMapper.readValue(responseBody, typeReference);
+        }
+
+        @Test
+        @DisplayName("Basic tests")
+        void allOpenReviews() {
+            assertAll(
+                    () -> request.andExpect(MockMvcResultMatchers.status().isOk()),
+                    () -> request.andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3)),
+                    () -> assertEquals(3, results.size()),
+                    () -> assertEquals(3, results.stream().distinct().count())
+            );
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {0, 1, 2})
+        @DisplayName("IDs tests")
+        void allOpenReviewsIDs(int index) {
+            assertEquals(7L * index, results.get(index).id());
+        }
+
+        @Test
+        @DisplayName("Complex test")
+        void allValuesAllOpenReviews() {
+            assertAll(
+                    () -> assertEquals("How to concat string in for statement", results.get(0).title()),
+                    () -> assertEquals("StringBuilder", results.get(0).javaFile()),
+                    () -> assertTrue(results.get(2).testFile().isEmpty()),
+                    () -> assertEquals("@Test", results.get(1).testFile()),
+                    () -> assertEquals(118218L, results.get(0).authorID()),
+                    () -> assertEquals(3630L, results.get(1).authorID()),
+                    () -> assertEquals(17L, results.get(2).authorID())
+            );
+        }
+
+    }
 //
 //    @Nested
 //    @Tag("POST")
