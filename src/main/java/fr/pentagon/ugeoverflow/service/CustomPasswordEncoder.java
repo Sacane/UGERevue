1d175e9b5e62a759df72cdf8a726c9a518d22de2
Verestah <mathis.menaa@edu.univ-eiffel.fr>
2024-02-19 10:50:08 +0100

    [UPDATE] Fix CustomPasswordEncoder problem.

--- src/main/java/fr/pentagon/ugeoverflow/service/CustomPasswordEncoder.java
+++ src/main/java/fr/pentagon/ugeoverflow/service/CustomPasswordEncoder.java
@@ -36,6 +36,21 @@ public final class CustomPasswordEncoder implements PasswordEncoder {
         }
     }
 
+    public String encodePassword(String password){
+        try {
+            var salt = generateSalt();
+            var md = MessageDigest.getInstance(ENCODING);
+            var hashPassword = md.digest((password + new String(salt)).getBytes(StandardCharsets.UTF_8));
+            var builder = new StringBuilder();
+            for (var b : hashPassword) { //Conversion into hexadecimal for a better readability and manipulation.
+                builder.append(String.format("%02x", b));
+            }
+            return builder + new String(salt);
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssertionError("SHA-256 algorithm doesn't exists anymore");
+        }
+    }
+
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
         var salt = encodedPassword.substring(ENCODING_LENGTH);
@@ -46,7 +61,22 @@ public final class CustomPasswordEncoder implements PasswordEncoder {
             for (var b : enteredPwdHash) {
                 builder.append(String.format("%02x", b));
             }
-            return builder.toString().equals(rawPassword.toString().substring(0,64));
+            return builder.toString().equals(encodedPassword.substring(0,64));
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssertionError("SHA-256 algorithm doesn't exists anymore");
+        }
+    }
+
+    public boolean verifyPassword(String enteredPwd, String storedPwd){
+        var salt = storedPwd.substring(ENCODING_LENGTH);
+        try {
+            var md = MessageDigest.getInstance(ENCODING);
+            var enteredPwdHash = md.digest((enteredPwd + salt).getBytes(StandardCharsets.UTF_8));
+            var builder = new StringBuilder();
+            for (var b : enteredPwdHash) {
+                builder.append(String.format("%02x", b));
+            }
+            return builder.toString().equals(storedPwd.substring(0,64));
         } catch (NoSuchAlgorithmException e) {
             throw new AssertionError("SHA-256 algorithm doesn't exists anymore");
         }
