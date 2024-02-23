33d5ac5c132a2a3acc13b6367696304086b62c55
qtdrake <quentintellier01@yahoo.com>
2024-02-23 21:28:58 +0100

    Add delete button on review details page

--- src/main/revevue-front/src/app/modules/reviews/reviews.component.ts
+++ src/main/revevue-front/src/app/modules/reviews/reviews.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ReviewDialogComponent, ReviewService } from '../../shared';
-import { BehaviorSubject, Subject, catchError, of, switchMap, takeUntil } from 'rxjs';
+import { BehaviorSubject, Subject, catchError, concat, of, switchMap, takeUntil } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { UserService } from '../../shared/HttpServices';
+import { Role } from '../questions/models/role.model';
+import { ConfirmDialogComponent } from '../../shared/components/confirm-dialog/confirm-dialog.component';
+import { Location } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-review-detail',
@@ -12,12 +16,14 @@ import { UserService } from '../../shared/HttpServices';
     encapsulation: ViewEncapsulation.None
 })
 export class ReviewsComponent implements OnDestroy {
+    deleting: boolean = false;
+    canDelete: boolean = false;
     canReview: boolean;
     review$ = new BehaviorSubject<any>(null);
     private reviewId: string;
     private _onDestroy = new Subject<void>();
 
-    constructor(private router: Router, private activatedRoute: ActivatedRoute, private userService: UserService, private reviewService: ReviewService, private dialog: MatDialog) {
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, private userService: UserService, private reviewService: ReviewService, private dialog: MatDialog, private snackBar: MatSnackBar, private location: Location) {
         this.reviewId = this.activatedRoute.snapshot.params['id'];
         this.canReview = this.userService.getLogin() !== '';
 
@@ -40,6 +46,7 @@ export class ReviewsComponent implements OnDestroy {
 
         this.reviewService.getDetails(this.reviewId).subscribe(response => {
             this.review$.next(response);
+            this.canDelete = response.author === this.userService.getLogin() || this.userService.getRole() === Role.ADMIN;
         });
     }
 
@@ -66,6 +73,44 @@ export class ReviewsComponent implements OnDestroy {
                 const review = this.review$.getValue();
 
                 this.review$.next({ ...review, reviews: [...review.reviews, response] });
+            }
+        });
+    }
+
+    deleteReview(): void {
+        this.dialog.open(ConfirmDialogComponent, {
+            data: {
+                title: 'Delete a review',
+                message: 'Confirm that you want to delete this review ?'
+            },
+            disableClose: true
+        }).afterClosed().pipe(
+            switchMap(confirm => {
+                if (confirm) {
+                    return concat(
+                        of({ deleting: true }),
+                        this.reviewService.deleteReview(this.review$.getValue().id).pipe(
+                            catchError(err => {
+                                console.log(err);
+                                return of({ error: err });
+                            })
+                        )
+                    );
+                }
+
+                return of();
+            })
+        ).subscribe(response => {
+            if (response && response.deleting) {
+                this.deleting = true;
+            }
+            else if (response && response.error) {
+                this.deleting = false;
+                this.snackBar.open('Error occurs while deleting the review', 'OK');
+            }
+            else {
+                this.snackBar.open('The review have been deleted', 'OK');
+                this.location.back();
             }
         });
     }
