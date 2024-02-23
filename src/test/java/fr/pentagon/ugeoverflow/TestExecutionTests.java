60b1e801094f1c69acf3112dcca42e3d52fae914
Sekelenao <Sekelenao@protonmail.com>
2024-02-24 00:51:28 +0100

    Added tests for TestTracker class

--- src/test/java/fr/pentagon/ugeoverflow/TestExecutionTests.java
+++ src/test/java/fr/pentagon/ugeoverflow/TestExecutionTests.java
@@ -2,10 +2,10 @@ package fr.pentagon.ugeoverflow;
 
 import fr.pentagon.ugeoverflow.exception.CompilationException;
 import fr.pentagon.ugeoverflow.utils.CustomTestClassLoader;
+import fr.pentagon.ugeoverflow.utils.TestTracker;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
@@ -21,32 +21,32 @@ final class TestExecutionTests {
 
     private static final String DEPENDENCY_FILE_NAME = "HelloWorld.java";
 
+    @BeforeEach
+    void initializeTempDirectory() throws IOException {
+        var f1 = Paths.get("src", "test", "resources", "FakeJavaFiles", "HelloWorld.java");
+        var f2 = Paths.get("src", "test", "resources", "FakeJavaFiles", "HelloWorldTest.java");
+        var d1 = Paths.get("src", "test", "resources", "TempDoNotRemove", "HelloWorld.java");
+        var d2 = Paths.get("src", "test", "resources", "TempDoNotRemove", "HelloWorldTest.java");
+        Files.copy(f1, d1);
+        Files.copy(f2, d2);
+    }
+
+    @AfterEach
+    void cleanTempDirectory() throws IOException {
+        try (var files = Files.list(TEST_DIRECTORY)) {
+            files.forEach(file -> {
+                try {
+                    Files.delete(file);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
     @Nested
     @DisplayName("CustomTestClassLoader")
     final class CustomTestClassLoaderTests {
-
-        @BeforeEach
-        void initializeTempDirectory() throws IOException {
-            var f1 = Paths.get("src", "test", "resources", "FakeJavaFiles", "HelloWorld.java");
-            var f2 = Paths.get("src", "test", "resources", "FakeJavaFiles", "HelloWorldTest.java");
-            var d1 = Paths.get("src", "test", "resources", "TempDoNotRemove", "HelloWorld.java");
-            var d2 = Paths.get("src", "test", "resources", "TempDoNotRemove", "HelloWorldTest.java");
-            Files.copy(f1, d1);
-            Files.copy(f2, d2);
-        }
-
-        @AfterEach
-        void cleanTempDirectory() throws IOException {
-            try (var files = Files.list(TEST_DIRECTORY)) {
-                files.forEach(file -> {
-                    try {
-                        Files.delete(file);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-        }
 
         @Test
         @DisplayName("'load' and 'clean' are working")
@@ -88,4 +88,40 @@ final class TestExecutionTests {
         }
     }
 
+    @Nested
+    @DisplayName("TestTracker")
+    final class TestTrackerTests {
+
+        @Test
+        @DisplayName("TestTracker is working")
+        void testTrackerWorking() throws CompilationException, IOException, ClassNotFoundException {
+            var loader = CustomTestClassLoader.in(TEST_DIRECTORY);
+            var testClass = loader.load(TEST_FILE_NAME, DEPENDENCY_FILE_NAME);
+            var tracker = TestTracker.runAndTrack(testClass);
+            var dummyTestTracker = TestTracker.runAndTrack(UgeOverflowApplication.class);
+            System.out.println(dummyTestTracker.failedTestsDetails());
+            var expectedDetail = """
+                    Failed tests :
+                                        
+                    	- wrongTest() : org.opentest4j.AssertionFailedError: expected: <Hello world ?> but was: <Hello world !>
+                    """;
+            assertAll("TestTracker is working",
+                    () -> assertFalse(tracker.allTestsPassed()),
+                    () -> assertEquals(1, tracker.failedTestsCount()),
+                    () -> assertEquals(1, tracker.passedTestsCount()),
+                    () -> assertEquals(expectedDetail, tracker.failedTestsDetails()),
+                    () -> assertTrue(dummyTestTracker.allTestsPassed()),
+                    () -> assertEquals(0, dummyTestTracker.passedTestsCount()),
+                    () -> assertEquals(0, dummyTestTracker.failedTestsCount()),
+                    () -> assertTrue(dummyTestTracker.failedTestsDetails().isEmpty())
+            );
+        }
+
+        @Test
+        @DisplayName("TestTracker assertions")
+        void testTrackerAssertions() {
+            assertThrows(NullPointerException.class, () -> TestTracker.runAndTrack(null));
+        }
+
+    }
 }
