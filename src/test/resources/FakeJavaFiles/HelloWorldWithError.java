6265183b7638e4cf649b99c0258a11a0a1a12616
Sekelenao <Sekelenao@protonmail.com>
2024-02-24 00:29:36 +0100

    Add a new fake java file for tests

---
+++ src/test/resources/FakeJavaFiles/HelloWorldWithError.java
@@ -0,0 +1,12 @@
+package fr.uge.fakestringprovider;
+
+public final class HelloWorld {
+    private static final String HELLO_WORLD = "Hello world !";
+
+    public HelloWorld() {
+    }
+
+    public String helloWorld() {
+        return HELL_WORLD;
+    }
+}
