d84a0ae40750ec0830231552d4fc4ea0a454e7e3
Clément Gaudet <derkeethus96@gmail.com>
2024-03-07 17:59:39 +0100

    Create profile info component

--- RevevueApp/src/main/revevue-front/src/app/modules/profile/components/profile/profile.component.ts
+++ RevevueApp/src/main/revevue-front/src/app/modules/profile/components/profile/profile.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatTab, MatTabContent, MatTabGroup} from "@angular/material/tabs";
+import {ProfileInfoComponent} from "../info/profile-info.component";
 
 @Component({
-  selector: 'app-profile',
-  standalone: true,
-  imports: [],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+    selector: 'app-profile',
+    standalone: true,
+    imports: [
+        MatTabGroup,
+        MatTab,
+        MatTabContent,
+        ProfileInfoComponent
+    ],
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
 
