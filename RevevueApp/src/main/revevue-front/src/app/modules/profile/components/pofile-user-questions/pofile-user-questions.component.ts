5e67981a72981cedbfb04bfd26d715f7eb32c016
Clément Gaudet <derkeethus96@gmail.com>
2024-03-08 13:48:58 +0100

    Add profile info user questions component

---
+++ RevevueApp/src/main/revevue-front/src/app/modules/profile/components/pofile-user-questions/pofile-user-questions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pofile-user-questions',
+  standalone: true,
+  imports: [],
+  templateUrl: './pofile-user-questions.component.html',
+  styleUrl: './pofile-user-questions.component.scss'
+})
+export class PofileUserQuestionsComponent {
+
+}
---
+++ RevevueApp/src/main/revevue-front/src/app/modules/profile/components/pofile-user-questions/profile-user-questions-content/profile-user-questions-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-user-questions-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-user-questions-content.component.html',
+  styleUrl: './profile-user-questions-content.component.scss'
+})
+export class ProfileUserQuestionsContentComponent {
+
+}
