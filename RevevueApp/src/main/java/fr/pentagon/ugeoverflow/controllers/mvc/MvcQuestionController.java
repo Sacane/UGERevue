12d3d397ddb0ec14af4667a58f040dd237d5c6e7
Sacane <johan.ramaroson@gmail.com>
2024-03-13 19:54:10 +0100

    We can now create a question in thymeleaf

--- RevevueApp/src/main/java/fr/pentagon/ugeoverflow/controllers/mvc/MvcQuestionController.java
+++ RevevueApp/src/main/java/fr/pentagon/ugeoverflow/controllers/mvc/MvcQuestionController.java
@@ -1,17 +1,25 @@
 package fr.pentagon.ugeoverflow.controllers.mvc;
 
-import fr.pentagon.ugeoverflow.controllers.dtos.responses.QuestionDTO;
+import fr.pentagon.ugeoverflow.config.authorization.RequireUser;
+import fr.pentagon.ugeoverflow.config.security.SecurityContext;
+import fr.pentagon.ugeoverflow.controllers.dtos.thymleaf.NewQuestionThymeleafDTO;
+import fr.pentagon.ugeoverflow.exception.HttpException;
 import fr.pentagon.ugeoverflow.service.QuestionService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/light/questions")
 public class MvcQuestionController {
     private final QuestionService questionService;
+    private final Logger logger = Logger.getLogger(MvcQuestionController.class.getName());
     public MvcQuestionController(QuestionService questionService) {
         this.questionService = questionService;
     }
@@ -23,10 +31,36 @@ public class MvcQuestionController {
         return "/pages/questions/detail";
     }
 
-    @GetMapping
+    @GetMapping("/")
     public String all(Model model) {
         var questions = questionService.getQuestions();
         model.addAttribute("questions", questions);
         return "/pages/questions/all";
+    }
+
+    @GetMapping("/ask")
+    public String askPage(@ModelAttribute("newQuestion") NewQuestionThymeleafDTO newQuestionDTO) {
+        if(SecurityContext.authentication().isEmpty()){
+            return "redirect:/light/forbidden";
+        }
+        return "/pages/questions/ask";
+    }
+
+    @PostMapping("/ask")
+    public String ask(@Valid @ModelAttribute("newQuestion") NewQuestionThymeleafDTO newQuestionDTO,
+                      BindingResult bindingResult, Authentication authentication
+    ){
+        logger.info("Post question : " + newQuestionDTO.javaFile().getOriginalFilename());
+        if(authentication == null) return "redirect:light/login";
+        if(bindingResult.hasErrors()) {
+            return "pages/questions/ask";
+        }
+        try {
+            questionService.create(newQuestionDTO.toNewQuestionDTO(), SecurityContext.checkAuthentication().id());
+        }catch (IOException e){
+            logger.severe(e.getMessage());
+            throw HttpException.badRequest("Le fichier n'a pas été correctement ouvert");
+        }
+        return "redirect:";
     }
 }
