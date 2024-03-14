cc7d670b35592bf1ffccee3352944d2d272d90fe
Sacane <johan.ramaroson@gmail.com>
2024-03-14 01:33:47 +0100

    Catch up angular with thymeleaf

--- RevevueApp/src/main/java/fr/pentagon/ugeoverflow/controllers/mvc/MvcReviewController.java
+++ RevevueApp/src/main/java/fr/pentagon/ugeoverflow/controllers/mvc/MvcReviewController.java
@@ -2,24 +2,34 @@ package fr.pentagon.ugeoverflow.controllers.mvc;
 
 import fr.pentagon.ugeoverflow.config.authentication.RevevueUserDetail;
 import fr.pentagon.ugeoverflow.config.security.SecurityContext;
+import fr.pentagon.ugeoverflow.controllers.dtos.requests.QuestionReviewCreateBodyDTO;
+import fr.pentagon.ugeoverflow.controllers.dtos.requests.QuestionReviewCreateDTO;
+import fr.pentagon.ugeoverflow.controllers.dtos.requests.ReviewOnReviewDTO;
+import fr.pentagon.ugeoverflow.controllers.dtos.responses.QuestionDetailsDTO;
+import fr.pentagon.ugeoverflow.service.QuestionService;
 import fr.pentagon.ugeoverflow.service.ReviewService;
 import fr.pentagon.ugeoverflow.utils.MarkdownRenderer;
+import jakarta.annotation.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Controller
 @RequestMapping("/light/reviews")
 public class MvcReviewController {
-    record SubReviewAuthorContent(long id, String author, String content){}
+    public record SubReviewAuthorContent(long id, String author, String content){}
     private final ReviewService reviewService;
+    private final QuestionService questionService;
     private final MarkdownRenderer markdownRenderer;
-
-    public MvcReviewController(ReviewService reviewService, MarkdownRenderer markdownRenderer) {
+    public record ReviewBodyDTO(@NotNull @NotBlank String content, @Nullable Integer lineStart, @Nullable Integer lineEnd) {
+    }
+    public MvcReviewController(ReviewService reviewService, MarkdownRenderer markdownRenderer, QuestionService questionService) {
         this.reviewService = reviewService;
         this.markdownRenderer = markdownRenderer;
+        this.questionService = questionService;
     }
 
     @GetMapping("/{reviewId}")
@@ -30,5 +40,19 @@ public class MvcReviewController {
         model.addAttribute("content", markdownRenderer.markdownToHtml(reviews.content()));
         model.addAttribute("subReviews", reviews.reviews().stream().map(e -> new SubReviewAuthorContent(e.id(), e.author(), markdownRenderer.markdownToHtml(e.content()))));
         return "pages/reviews/detail";
+    }
+
+    @GetMapping("/create/{questionId}")
+    public String createReviewPage(@ModelAttribute("body") ReviewBodyDTO questionReviewCreateDTO, @PathVariable("questionId") long questionId, Model model){
+        model.addAttribute("request", new QuestionReviewCreateBodyDTO(questionId, questionReviewCreateDTO.content(), questionReviewCreateDTO.lineStart(), questionReviewCreateDTO.lineEnd()));
+        return "pages/reviews/create";
+    }
+
+    @PostMapping("/create")
+    public String createReview( @ModelAttribute("request") QuestionReviewCreateBodyDTO questionReviewCreateDTO) {
+        System.out.println("et la ");
+        var currentUser = SecurityContext.checkAuthentication();
+        questionService.addReview(new QuestionReviewCreateDTO(currentUser.id(), questionReviewCreateDTO.questionId(), questionReviewCreateDTO.content(), questionReviewCreateDTO.lineStart(), questionReviewCreateDTO.lineEnd()));
+        return "redirect:../../light/questions/" + questionReviewCreateDTO.questionId();
     }
 }
