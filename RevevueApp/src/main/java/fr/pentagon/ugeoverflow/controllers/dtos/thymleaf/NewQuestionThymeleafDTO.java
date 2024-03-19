6ad4a4b239250f83e598c3e121bb8aadd826b1d6
Sacane <johan.ramaroson@gmail.com>
2024-03-19 13:45:04 +0100

    resolve dependencies

--- RevevueApp/src/main/java/fr/pentagon/ugeoverflow/controllers/dtos/thymleaf/NewQuestionThymeleafDTO.java
+++ RevevueApp/src/main/java/fr/pentagon/ugeoverflow/controllers/dtos/thymleaf/NewQuestionThymeleafDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import java.io.IOException;
 
 public record NewQuestionThymeleafDTO(
